7cc9d65a2a8299fa8c05359139aa5c75e2c942c3
Work <joshuablaine@users.noreply.github.com>
2016-08-27 15:56:27 -0700

    initial commit

---
+++ js/main.jsx
@@ -0,0 +1,7 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<span>test</span>, rootEl);
+});
